1a39a071839795091ee8b409b989831544c541a1
nouveau-riche <2019kucp1049@iiitkota.ac.in>
2021-01-20 10:59:31 +0530

    project init

---
+++ android/app/src/main/kotlin/com/nikunj/uberrider/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nikunj.uberrider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import './screens/home_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
